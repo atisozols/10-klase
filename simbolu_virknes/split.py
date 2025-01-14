f72ea0985a1a9c4a5bd05eaf5ae29cd41667c4b8
atisozols <atisozols5@gmail.com>
2025-01-14 14:40:13 +0200

    Create split.py

---
+++ simbolu_virknes/split.py
@@ -0,0 +1,29 @@
+# text = "tukuma raina valsts gimnazija"
+# words = text.split()
+# print(words)
+
+# rezultats = "89:112"
+# punkti = rezultats.split(":")
+
+# for komanda in punkti:
+#     print(komanda)
+
+# # originalais teksts
+# vertibasText = "12,4,23,9,31"
+
+# # teksts sadalits pa komatu -> ['12', '4', '23', '9', '31']
+# vertibas = vertibasText.split(",")
+
+# skaitli = []
+
+# for vertiba in vertibas:
+#     skaitli.append(int(vertiba))
+
+# skaitli.sort()
+
+# newOutput = ""
+
+# for skaitlis in skaitli:
+#     newOutput += str(skaitlis) + ","
+
+# print(newOutput[:-1])
